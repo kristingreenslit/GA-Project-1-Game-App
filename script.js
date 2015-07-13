a4817425bded0a7a4c54cb8c87c10b65ae1c5068
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-13 14:43:29 -0400

    add vertical-align property to resolve issue with tiles not centering properly using float:left

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ $(document).ready (function(){
 user clicks on first square
 > event listener for button click
 
-first square content is revealed
-> on click reveal button content
+$(".tiles").on("click", function(){
 
-user clicks on second square
->event listener for button click
+})
 
-second square content is revealed
->on click reveal button content
-
-if squares match, player wins match and green checkmark appears over squares
->boolean for a winning play
->visually represent a successful match
-
-if squares don't match, both squares flip back to hidden state
->boolean for a non-winning play
->visually represent "try again"
+// first square content is revealed
+// > on click reveal button content
+//
+// user clicks on second square
+// >event listener for button click
+//
+// second square content is revealed
+// >on click reveal button content
+//
+// if squares match, player wins match and green checkmark appears over squares
+// >boolean for a winning play
+// >visually represent a successful match
+//
+// if squares don't match, both squares flip back to hidden state
+// >boolean for a non-winning play
+// >visually represent "try again"
 
 
 
