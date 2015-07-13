49ac253841eb3196f5c649f9d384a6439cf9771f
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-13 17:41:14 -0400

    js toggle class and fade in/fade out working properly

--- script.js
+++ script.js
@@ -1,12 +1,45 @@
 $(document).ready(function(){
 
+  // $( "button" ).hover(
+  //   function() {
+  //     $( this ).addClass( ".tiles1" );
+  //   }, function() {
+  //     $( this ).removeClass( ".tiles2" );
+  //   }
+  // );
+
+  $( "button" ).hover(function() {
+    $( this ).fadeOut( 1000 );
+    $( this ).fadeIn( 1000 );
+  });
+// var x = $("button").click();
+//
+  $( "#box1" ).toggleClass(function() {
+    if ( $( this ).parent().is( "#buttons1" ) ) {
+      console.log("happy");
+    } else {
+      console.log("sad");
+    }
+  });
+
 // user clicks on first square
 // > event listener for button click
 
-$(".tiles2").click(function(){
-  $(this).removeclass();
-  console.log("this");
-});
+// function randomColorChange(){
+//
+// }
+
+// var a = $
+// $("button").click(function(){
+//   $(".tiles1").addClass(".tiles2");
+//   $(".tiles1").removeClass(".tiles1");
+//   console.log(this);
+// });
+
+// $(".tiles2").click(function(){
+//   $(this).removeclass();
+//   console.log("this");
+// });
 
 // first square content is revealed
 // > on click reveal button content
