91fdc127ae207305566306b89a5656215226e3d2
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-14 12:56:26 -0400

    evaluate if tiles chosen are identical

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 $(document).ready(function(){
 
 var tilesChosen = [];
+var arr2 = [];
+var clickCounter = 0;
 
 $( "button" ).click(function() {
   $( this ).toggleClass( "tiles2" );
-  tilesChosen.push(this);
+  clickCounter++;
+  tilesChosen.push( this );
+  $( this ).attr("disabled", "disabled");
   console.log(tilesChosen);
 });
 
-// for (i = 0; i < 2; i++) {
-//     console.log("hi");
+
+
+for (var i = 0; i <= tilesChosen.length; i++){
+  checkChoices = tilesChosen[i];
+  if ( checkChoices[i].Id == arr2[j].Id ){
+    console.log("yes")
+  } else console.log("no");
+
+};
+  // if ( $( this ).hasClass( tiles2[i] )
+
+
+// var match = $("#id1").html() === $("#id2").html();
+// function determineMatch(){
+//   if ()
 // }
-
-
-
-
 
 
 });
