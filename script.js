3c62d344f691d1a3d0b55332b336aafdcb99d175
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-14 10:18:40 -0400

    CSS and js changes for new approach to toggling classes on selected tile, condensed HTML

--- script.js
+++ script.js
@@ -1,60 +1,17 @@
 $(document).ready(function(){
 
-  // $( "button" ).hover(
-  //   function() {
-  //     $( this ).addClass( ".tiles1" );
-  //   }, function() {
-  //     $( this ).removeClass( ".tiles2" );
-  //   }
-  // );
+var tilesChosen = 0;
 
-  $( "button" ).hover(function() {
-    $( this ).fadeOut( 1000 );
-    $( this ).fadeIn( 1000 );
-  });
-// var x = $("button").click();
-//
-  $( "#box1" ).toggleClass(function() {
-    if ( $( this ).parent().is( "#buttons1" ) ) {
-      console.log("happy");
-    } else {
-      console.log("sad");
-    }
-  });
+$( "button" ).click(function() {
+  $( this ).toggleClass( "tiles2" );
+  tilesChosen++;
+  console.log(tilesChosen);
+  // for (i = 0, i < 2, i++);
+});
 
-// user clicks on first square
-// > event listener for button click
 
-// function randomColorChange(){
-//
-// }
 
-// var a = $
-// $("button").click(function(){
-//   $(".tiles1").addClass(".tiles2");
-//   $(".tiles1").removeClass(".tiles1");
-//   console.log(this);
-// });
 
-// $(".tiles2").click(function(){
-//   $(this).removeclass();
-//   console.log("this");
-// });
 
-// first square content is revealed
-// > on click reveal button content
-//
-// user clicks on second square
-// >event listener for button click
-//
-// second square content is revealed
-// >on click reveal button content
-//
-// if squares match, player wins match and green checkmark appears over squares
-// >boolean for a winning play
-// >visually represent a successful match
-//
-// if squares don't match, both squares flip back to hidden state
-// >boolean for a non-winning play
-// >visually represent "try again"
+
 });
