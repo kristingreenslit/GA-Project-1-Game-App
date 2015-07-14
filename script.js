55d79298646626a5ab6b58c4b373612d6d3f0bd8
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-14 19:12:45 -0400

    properly evaluating matched vs. non-matched tiles, issue persists with resetting non-matched tiles

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 $(document).ready(function(){
 
-var tilesChosen = [];
-var clickCounter = 0;
+  var clickCounter = 0;
+  var actualClicks = [];
 
-$( "button" ).click(function() {
-  $( this ).toggleClass( "tiles2" );
-  clickCounter++;
-  tilesChosen.push( this );
-  $( this ).attr("disabled", "disabled");
-  console.log(tilesChosen);
-  for (var i = 0; i <= tilesChosen.length; i++){
-    if ( $( tilesChosen[i] ).hasClass( "tiles2"[i] )){
-      console.log("yes");
-    } else console.log("no");
-  }
+  $( "button" ).click(function() {
+    $( this ).addClass( "tiles2" );
+    $( this ).removeClass( "tiles1" );
+    clickCounter++;
+    actualClicks.push($( this ).text());
+    if (clickCounter % 2 === 0){
+      if (actualClicks[0] !== actualClicks[1]) {
+        console.log("try again!");
+        $( this ).addClass( "tiles2" );
+        $( this ).removeClass( "tiles1" );
+        console.log(this);
+      }
+      else if (actualClicks[0] === actualClicks[1]) {
+          console.log("you win!");
+          actualClicks = [];
+          $( this ).attr("disabled", "disabled");
+      }
+    };
+  });
 });
 
-});
+  // if clickcounter is 1 just flip dont do anything else\
+
+  // if clickcounter is even
+    // test to see if its a match, if it is
+      // do nothing
+    // if its not a match we want to flip the cards back over
+
+  // if its odd
+    // just flip
