53ef4c5e642cf85ea2420f0d1c19c7d18681aace
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-14 13:22:06 -0400

    loop through elements in array to evaluate if tiles chosen are identical

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
 var tilesChosen = [];
-var arr2 = [];
 var clickCounter = 0;
 
 $( "button" ).click(function() {
@@ -10,24 +9,11 @@ $( "button" ).click(function() {
   tilesChosen.push( this );
   $( this ).attr("disabled", "disabled");
   console.log(tilesChosen);
+  for (var i = 0; i <= tilesChosen.length; i++){
+    if ( $( tilesChosen[i] ).hasClass( "tiles2"[i] )){
+      console.log("yes");
+    } else console.log("no");
+  }
 });
-
-
-
-for (var i = 0; i <= tilesChosen.length; i++){
-  checkChoices = tilesChosen[i];
-  if ( checkChoices[i].Id == arr2[j].Id ){
-    console.log("yes")
-  } else console.log("no");
-
-};
-  // if ( $( this ).hasClass( tiles2[i] )
-
-
-// var match = $("#id1").html() === $("#id2").html();
-// function determineMatch(){
-//   if ()
-// }
-
 
 });
