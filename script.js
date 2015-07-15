c5355f9171d26d4e2919b66ef668c4b3b2d4cf83
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-15 13:30:35 -0400

    2nd attempt at new solution

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
   var clickCounter = 1;
   var tileChoiceText = [];
@@ -7,50 +7,47 @@
 
 $( "button" ).click(function() {
     tileChoiceText.push($(this).text());
+    // console.log(tileChoiceText);
     $( this ).attr("disabled", "disabled");
     $( this ).addClass( "tiles2" );
     $( this ).removeClass( "tiles1" );
     console.log(this);
       if (clickCounter % 2 === 1) {
         tileChoice1 = $(this).attr("id");
+        console.log(tileChoice1);
         clickCounter++;
       }
       else if (clickCounter % 2 === 0){
         tileChoice2 = $(this).attr("id");
+        console.log(tileChoice2);
           if (tileChoiceText[0] !== tileChoiceText[1]) {
-             tileChoice1.addClass( "tiles1" );
-             tileChoice1.removeClass( "tiles2" );
-             tileChoice2.addClass( "tiles1" );
-             tileChoice2.removeClass( "tiles2" );
-            alert("try again!");
-          }
+             $(tileChoice1).addClass( "tiles1" ).removeClass( "tiles2" );
+            //  $(tileChoice2).addClass( "tiles1" ).removeClass( "tiles2" );
+             alert("try again!");
+             tileChoiceText = [];
+             clickCounter++;
+            //  console.log(tileChoiceText);
+          }  else if (tileChoiceText[0] === tileChoiceText[1]) {
+            tileChoiceText = [];
+            clickCounter++;
+
+                 alert("you win!");
+              }
         }
-      });
-
-    console.log(tileChoiceText);
-
-        else if (tileChoiceText[0] === tileChoiceText[1]) {
-           console.log("you win!");
-           tileChoiceText = [];
-           alert("you win!");
-        }
-
-      });
-
       });
 
  });
 
 
 
-    // $("#reset").click(window.onload);
-    // window.onload = rearrangeTiles;
-    //
-    //  function rearrangeTiles() {
-    //     var scrambleTiles = $(".tiles1");
-    //     for(var i = 0; i < scrambleTiles.length; i++){
-    //     var target = Math.floor(Math.random() * scrambleTiles.length -1) + 1;
-    //     var target2 = Math.floor(Math.random() * scrambleTiles.length -1) +1;
-    //     scrambleTiles.eq(target).before(scrambleTiles.eq(target2));
-    //     }
-    //   }
+    $("#reset").click(window.onload);
+    window.onload = rearrangeTiles;
+
+     function rearrangeTiles() {
+        var scrambleTiles = $(".tiles1");
+        for(var i = 0; i < scrambleTiles.length; i++){
+        var target = Math.floor(Math.random() * scrambleTiles.length -1) + 1;
+        var target2 = Math.floor(Math.random() * scrambleTiles.length -1) +1;
+        scrambleTiles.eq(target).before(scrambleTiles.eq(target2));
+        }
+      }
