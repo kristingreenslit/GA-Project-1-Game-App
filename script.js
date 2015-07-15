572a3c1625446d79030e594bdafccfe354664c1a
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-15 10:59:19 -0400

    global variable added to change background color of mismatched tiles but issue still persists

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
 $(document).ready(function(){
 
   var clickCounter = 0;
-  var tilesChosen = [];
+  var tileChoices = [];
+  var TileChosen;
 
 $( "button" ).click(function() {
+    var TileChosen = this;
     $( this ).addClass( "tiles2" );
     $( this ).removeClass( "tiles1" );
     clickCounter++;
-    tilesChosen.push($( this ).html());
+    tileChoices.push($( this ).html());
     $( this ).attr("disabled", "disabled");
       // if ($(".tiles2").attr("disabled", "disabled")){
       //   window.location.reload(true);
       // }
-        if (tilesChosen[0] === tilesChosen[1]) {
+        if (tileChoices[0] === tileChoices[1]) {
            console.log("you win!");
-           tilesChosen = [];
+           tileChoices = [];
            alert("you win!");
         } else if (clickCounter % 2 === 0){
-           if (tilesChosen[0] !== tilesChosen[1]) {
-        // $( tilesChosen ).addClass( "tiles2" );
-           $( ".tiles1" ).removeClass(".tiles2");
-           alert("try again!");
-        };
-           console.log(tilesChosen);
-      }
-    });
-  });
+           if (tileChoices[0] !== tileChoices[1]) {
+             $("TileChosen").click( function() {
+               $(this).css("background", "red");
+              });
+            alert("try again!");
+          }}
+        });
+      });
+
+
 
     // $("#reset").click(window.onload);
-    window.onload = rearrangeTiles;
-
-     function rearrangeTiles() {
-        var scrambleTiles = $(".tiles1");
-        for(var i = 0; i < scrambleTiles.length; i++){
-        var target = Math.floor(Math.random() * scrambleTiles.length -1) + 1;
-        var target2 = Math.floor(Math.random() * scrambleTiles.length -1) +1;
-        scrambleTiles.eq(target).before(scrambleTiles.eq(target2));
-        }
-      }
+    // window.onload = rearrangeTiles;
+    //
+    //  function rearrangeTiles() {
+    //     var scrambleTiles = $(".tiles1");
+    //     for(var i = 0; i < scrambleTiles.length; i++){
+    //     var target = Math.floor(Math.random() * scrambleTiles.length -1) + 1;
+    //     var target2 = Math.floor(Math.random() * scrambleTiles.length -1) +1;
+    //     scrambleTiles.eq(target).before(scrambleTiles.eq(target2));
+    //     }
+    //   }
