db12a1b053452c776965235659cb6118048eced1
KristinGreenslit <kristingreenslit@gmail.com>
2015-08-01 17:14:21 -0400

    tiles flipping after two clicks

--- script.js
+++ script.js
@@ -2,36 +2,51 @@ $(document).ready(function(){
 
   var clickCounter = 0;
   var actualClicks = [];
+  var elementsClicked = [];
 
 $( "button" ).click(function() {
+    actualClicks.push($( this ).html());
+    $( this ).attr("disabled", "disabled"); // prevents user from accidental click on the same tile
     $( this ).addClass( "tiles2" );
     $( this ).removeClass( "tiles1" );
-    clickCounter++;
-    actualClicks.push($( this ).text());
-    $( this ).attr("disabled", "disabled");
-    if (clickCounter % 2 === 0){
-      if (actualClicks[0] !== actualClicks[1]) {
-        $( this ).addClass( "tiles2" );
-        $( this ).removeClass( "tiles1" );
-        alert("try again!");
-      }
-      else if (actualClicks[0] === actualClicks[1]) {
-        console.log("you win!");
-        actualClicks = [];
-        alert("you win!");
-      }
+    elementsClicked.push( this );
+    if (clickCounter === 0){
+
     }
+    if (clickCounter === 1){
+         clickCounter = -1;
+         if (actualClicks[0] !== actualClicks[1]) {
+             setTimeout(flipBack, 1000);
+             console.log("try again!");
+         } else if (actualClicks[0] === actualClicks[1]) {
+             console.log("you win!");
+             $(elementsClicked[1]).css("visibility","hidden");
+             $(elementsClicked[0]).css("visibility","hidden");
+             actualClicks = [];
+             elementsClicked = [];
+         }
+    }
+    clickCounter++;
   });
 
-    // $("#reset").click(window.onload);
-    window.onload = rearrangeTiles;
+  function flipBack(){
+    $(elementsClicked[0]).addClass("tiles1").removeClass("tiles2").removeAttr("disabled");
+    $(elementsClicked[1]).addClass("tiles1").removeClass("tiles2").removeAttr("disabled");
+    actualClicks = [];
+    elementsClicked = [];
+  }
 
-     function rearrangeTiles() {
-        var scrambleTiles = $(".tiles1");
-        for(var i = 0; i < scrambleTiles.length; i++){
-        var target = Math.floor(Math.random() * scrambleTiles.length -1) + 1;
-        var target2 = Math.floor(Math.random() * scrambleTiles.length -1) +1;
-        scrambleTiles.eq(target).before(scrambleTiles.eq(target2));
-        }
-      }
+
+    // $("#reset").click(window.onload);
+//     window.onload = rearrangeTiles;
+//
+//      function rearrangeTiles() {
+//         var scrambleTiles = $(".tiles1");
+//         var quantityTiles = scrambleTiles.length -1;
+//         for(var i = 0; i < scrambleTiles.length; i++){
+//         var target = Math.floor(Math.random() * quantityTiles) + 1;
+//         var target2 = Math.floor(Math.random() * quantityTiles) + 1;
+//         scrambleTiles.eq(target).before(scrambleTiles.eq(target2));
+//         }
+//       }
 });
